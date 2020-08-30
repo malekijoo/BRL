dae159daf676d3a81e5f2657b56a593b16e4b2f5
amir <amirhossein.maleki1990@gmail.com>
2020-08-30 19:01:33 +0430

    extractor: return dictionary {'deltatime: change percent, each day change percent than yesterday'}

--- utility.py
+++ utility.py
@@ -6,41 +6,39 @@ from sklearn.preprocessing import OneHotEncoder
 
 
 def one_hot(action):
+    act_df = pd.DataFrame(action, columns=['actions'])
+    act_df['actions_type'] = act_df['actions'].astype('category')
+    act_df['actions_type_num'] = act_df['actions_type'].cat.codes
 
-  act_df = pd.DataFrame(action, columns=['actions'])
-  act_df['actions_type'] = act_df['actions'].astype('category')
-  act_df['actions_type_num'] = act_df['actions_type'].cat.codes
+    # creating instance of one-hot-encoder
+    enc = OneHotEncoder(handle_unknown='ignore')
+    enc_df = pd.DataFrame(enc.fit_transform(act_df[['actions_type_num']]).toarray())
+    act_df = act_df.join(enc_df).drop(columns=['actions_type'])
+    act_df = act_df.set_index(['actions_type_num', 'actions'])
 
-  # creating instance of one-hot-encoder
-  enc = OneHotEncoder(handle_unknown='ignore')
-  enc_df = pd.DataFrame(enc.fit_transform(act_df[['actions_type_num']]).toarray())
-  act_df = act_df.join(enc_df).drop(columns=['actions_type'])
-  act_df = act_df.set_index(['actions_type_num', 'actions'])
+    return act_df.sort_index()
 
-  return act_df.sort_index()
 
 def detect_action(a):
+    action = ('Buy', 'Sell', 'Pass')
+    t = one_hot(action)
+    a = pd.Series(a).argmax()
 
-  action = ('Buy', 'Sell', 'Pass')
-  t = one_hot(action)
-  a = pd.Series(a).argmax()
-
-  return t.loc[a].values[0], t.loc[a].index.values
+    return t.loc[a].values[0], t.loc[a].index.values
 
 
 def extract(df, idx):
+    datetime = pd.to_datetime(df.iloc[idx][18])
+    price = df.iloc[idx][1]
+    name = df.iloc[idx][0]
+    target = df[df['name'] == name]
+    future_price = []
+    for target_date, obs in target.groupby(by=target['datetime'].values.astype('<M8[D]')):
+        deltatime = pd.Timedelta(pd.to_datetime(obs.iloc[0]['datetime']).date() - datetime.date()).days
+        if 0 < deltatime:
+            if 20 > deltatime:
+                future_price.append({'{}'.format(deltatime):
+                                         (((obs['price'].mean() - price) / 100 + (
+                                                     obs['ydp'].mean() - price) / 100) / 2)})
 
-  datetime = pd.to_datetime(df.iloc[idx][18])
-  price = df.iloc[idx][1]
-  name = df.iloc[idx][0]
-  target = df[df['name'] == name]
-  future_price = []
-  for target_date, obs in target.groupby(by=target['datetime'].values.astype('<M8[D]')):
-    deltatime = pd.Timedelta(pd.to_datetime(obs.iloc[0]['datetime']).date() - datetime.date()).days
-    if 0 < deltatime:
-      if 20 > deltatime:
-        future_price.append({'{}'.format(deltatime): [(obs['price'].mean()-price)/100, (obs['ydp'].mean()-price)/100]})
-
-  return future_price  # [delta day, mean() all the day price ]
-
-
+    return future_price  # [delta day, mean() all the day price]
